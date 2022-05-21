21e241f8fb79da85e6144722513ebaac1cf488ea
hphph <hphph2@gmail.com>
2022-05-21 21:15:05 +0200

    pong test

--- js/pong-game.js
+++ js/pong-game.js
@@ -19,17 +19,30 @@ const config = {
 
 var game = new Phaser.Game(config);
 var paddle;
+var ball;
 var blocks;
 
 
 function preload()
 {
     this.load.image('background', 'assets/background600x800.png');
+    this.load.image('paddle', 'assets/paddle.png');
+    this.load.image('yellow-block', 'yellow-block.png');
+    this.load.image('red-block', 'red-block.png');
+    this.load.image('pink-block', 'pink-block.png');
+    this.load.image('orange-block', 'orange-block.png');
+    this.load.image('lightblue-block', 'lightblue-block.png');
+    this.load.image('grey-block', 'grey-block.png');
+    this.load.image('darkblue-block', 'darkblue-block.png');
+    this.load.image('ball', 'ball.png');
 }
 
 function create()
 {
     this.add.image(300, 400, 'background');
+    paddle = this.physics.add.sprite(300, 700, 'paddle');
+    ball = this.physics.add.sprite(300, 400, 'ball');
+
 }
 
 function update()

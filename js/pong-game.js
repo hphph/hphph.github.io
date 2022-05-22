405ec0d23e104a466d680f370ae45407bdc78a89
hphph <hphph2@gmail.com>
2022-05-22 13:17:39 +0200

    this

--- js/pong-game.js
+++ js/pong-game.js
@@ -18,9 +18,6 @@ const config = {
 };
 
 var game = new Phaser.Game(config);
-var paddle;
-var ball;
-var blocks;
 var cursors;
 
 
@@ -41,22 +38,22 @@ function preload()
 function create()
 {
     this.add.image(300, 400, 'background');
-    paddle = this.physics.add.image(300, 750, 'paddle');
-    ball = this.physics.add.image(300, 400, 'ball').setCollideWorldBounds(true).setBounce(1);
-    blocks = this.physics.add.staticGroup();
+    this.paddle = this.physics.add.image(300, 750, 'paddle');
+    this.ball = this.physics.add.image(300, 400, 'ball').setCollideWorldBounds(true).setBounce(1);
+    this.blocks = this.physics.add.staticGroup();
 
     for(var j = 1; j <=7; j++)
     {
         for(var i = 0; i<8; i++)
         {
-            blocks.create(39 + 74*i, j*19 + 12, 'block' + j);
+            this.blocks.create(39 + 74*i, j*19 + 12, 'block' + j);
         }
     }
 
     this.physics.world.setBoundsCollision(true, true, true, false);
-    this.physics.add.collider(ball, paddle);
-    this.physics.add.collider(ball, blocks);
-    ball.setVelocity(-75, -300);
+    this.physics.add.collider(this.ball, this.paddle);
+    this.physics.add.collider(this.ball, this.blocks);
+    this.ball.setVelocity(-75, -300);
 
     cursors = this.input.keyboard.createCursorKeys();
 }
